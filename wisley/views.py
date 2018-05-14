d272fc05feb8eacedc4306e645c75863256d21f4
annie917 <annie_alexander@icloud.com>
2018-05-14 10:13:24 +0100

    Started adding input validation and extended exception handling.

--- wisley/views.py
+++ wisley/views.py
@@ -1,27 +1,50 @@
-from flask import Flask, request, Response, make_response
+from flask import Flask, request, make_response
+
 import wisley.bus_layer as bl
 from wisley.models import Node
+
 import jsonpickle
 
+from voluptuous import Schema, MultipleInvalid, Coerce, Required
+
+
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
 
 @app.route('/fbr')
 def flower_bed_route():
 
+    """Route takes
+    plant (plant name number, coercible to integer)
+    lat (latitude, coercible to float)
+    long (longitude, coercible to float)
+    Returns a Route object containing information about the route from the location given in
+    the lat and long parameters to the closest instance of the plant given in the plant parameter"""
+
+    # Define validation schema
+    schema = Schema({
+        Required('plant'): Coerce(int),
+        Required('lat'): Coerce(float),
+        Required('long'): Coerce(float),
+    })
+
+    # Validate and return a Bad Request error if necessary
     try:
-        f_bed_route = bl.get_flower_bed_route(request.args['plant'], Node(0, request.args['long'], request.args['lat'], ''))
-        resp = get_response(f_bed_route)
+        schema(request.args.to_dict())
 
-    except Exception as err:
-        resp = handle_exception(err)
+    except MultipleInvalid as err:
 
-    # f_bed_route = bl.get_flower_bed_route('72209', Node(0, '-0.8570765', '51.2914787', ''))
+        resp = handle_exception(err, '400')
+
+    else:
+
+        # Call business layer method and return an Internal Server Error if anything goes wrong
+        try:
+            f_bed_route = bl.get_flower_bed_route(request.args['plant'], Node(0, request.args['long'],
+                                                                              request.args['lat'], ''))
+            resp = get_response(f_bed_route)
+
+        except Exception as err:
+            resp = handle_exception(err, '500')
 
     return resp
 
@@ -101,17 +124,13 @@ def get_response(resp_obj):
     return resp
 
 
-def handle_exception(err):
+def handle_exception(err, code):
 
     print("Exception: ", err)
 
-    return get_err_response()
-
-
-def get_err_response():
-
-    resp = make_response()
-    resp.status = '500'
+    resp = make_response(jsonpickle.encode(str(err)))
+    resp.mimetype = 'application/json'
+    resp.status = code
 
     return resp
 
